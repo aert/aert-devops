520279083f1440973e7c42109295e6410bc58bb7
aert <dev.aert@gmail.com>
2013-05-20 20:57:44 +0200

    Forcing usage of fabfile_local.py

--- fabfile.py
+++ fabfile.py
@@ -1,13 +1,9 @@
 from fabric.api import *
 from fabric_tasks.decorators import task_with_pkg_log
-from fabric_tasks import hosts as h
+from fabric_tasks import hosts
 from fabric_tasks import setup, secure, apt
 from fabric_tasks import ssh, cmd, salt
-
-try:
-    __import__("fabfile_local")
-except ImportError:
-    print("No file fabric_local.py")
+import fabfile_local as h
 
 @task
 def uname():
